fccd552ff4438e30b0da1ad4d2e02f7262c0fbc0
ShibuyaKosuke <github@shibuyakosuke.com>
2022-08-14 11:59:26 +0900

    fix

---
+++ tests/TestCase.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace ShibuyaKosuke\LaravelErDiagram\Test;
+
+use Orchestra\Testbench\TestCase as OrchestraTestCase;
+use ShibuyaKosuke\LaravelJetAdminlte\Providers\EventServiceProvider;
+use ShibuyaKosuke\LaravelJetAdminlte\Providers\ServiceProvider;
+
+abstract class TestCase extends OrchestraTestCase
+{
+    /**
+     * @param \Illuminate\Foundation\Application $app
+     * @return string[]
+     */
+    protected function getPackageProviders($app)
+    {
+        return [
+            ServiceProvider::class,
+            EventServiceProvider::class,
+        ];
+    }
+}
